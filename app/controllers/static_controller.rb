b800d3177ed1fbd6998082a1490ac60906ef19ce
ee1922 <ee1922@st.sasebo.ac.jp>
2020-07-16 11:36:04 +0900

    lol

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def home
-    render json: { status: "It's Alfred's App"}
+    render json: { status: "Hey!!!! It's Alfred!!!! Hope you are having a great Day!!!!"}
   end
 end
